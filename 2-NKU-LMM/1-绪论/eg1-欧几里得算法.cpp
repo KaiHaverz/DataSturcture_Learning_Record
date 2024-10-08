e7455bc99592686abf5e807dd0f70aa803c0d815
KaiHaverz <837597708@qq.com>
2024-10-08 18:33:10 +0800

    update Oct 8

--- 2-NKU-LMM/1-绪论/eg1-欧几里得算法.cpp
+++ 2-NKU-LMM/1-绪论/eg1-欧几里得算法.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+//递归实现欧几里得算法，求最大公约数
+int gcd(int a,int b){
+    if(b==0){
+        return a;
+    }
+    else{
+        return gcd(b,a%b);
+    }
+}
 
+int main(){
+    int a,b;
+    cin>>a>>b;
+
+    cout<<gcd(a,b)<<endl;
+
+    return 0;
 }
